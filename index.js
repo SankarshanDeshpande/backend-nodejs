8da972eb3e3b7394e304aee648312007c46882b9
SankarshanDeshpande <sankarshan703@gmail.com>
2023-12-27 12:44:47 +0530

    new file:   controller/user.controller.js 	modified:   index.js

--- index.js
+++ index.js
@@ -4,32 +4,46 @@ const express = require('express');
 const app = express();
 const db = require('./db.config');
 const PORT = process.env.PORT || 5000;
-
+// controller import
+const userRouter = require('./controller/user.controller')
 
 app.use(express.json()); // middleware
 app.use(express.urlencoded({extended:true}))
 db.dbConfig();
 
-// const connectionParams={
-//     useNewUrlParser:true,
-//     useCreteIndex:true,
-//     useUnifiedTopology:true
-// }
-
-
-
 app.get('/',(req,res)=>{
     res.json({
         status: 200,
         message: "your server is up and running",
-        uri: uri
     })
 })
 
-app.get('/home',(req,res)=>{
-    res.json("this is a home router...")
-})
+app.use('/api/v1/users',userRouter)
+// app.use('/api/v1/orders')
+// app.use('/api/v1/employees')
+// app.use('/api/v1/blogs')
+// app.use('/api/v1/post')
 
 app.listen(PORT, ()=>{
     console.log(`listening on port number -> ${PORT}`)
 })
+
+// http://localhost:8787/api/v1/users/createuser
+// http://localhost:8787/api/v1/users/
+// http://localhost:8787/api/v1/users/update
+// http://localhost:8787/api/v1/users/delete
+// http://localhost:8787/api/v1/users/findone
+// http://localhost:8787/api/v1/users/deleteall
+// http://localhost:8787/api/v1/users/findactiveuser
+
+
+
+// users
+
+// create user
+// findall
+// update
+// delete
+// find one
+// delete all
+// find active users
